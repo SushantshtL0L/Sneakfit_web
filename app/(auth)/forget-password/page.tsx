e0d501d2bb3ce3ab4a655a811da96365d03203ec
SushantshtL0L <shtsushant2027@gmail.com>
2026-02-14 17:42:28 +0545

    Sprint-5: implement forget password page

---
+++ app/(auth)/forget-password/page.tsx
@@ -0,0 +1,11 @@
+import ForgetPasswordForm from "../_components/ForgetPasswordForm";
+
+export default function ForgetPasswordPage() {
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-gray-50 px-4">
+            <div className="w-full max-w-md">
+                <ForgetPasswordForm />
+            </div>
+        </div>
+    );
+}
