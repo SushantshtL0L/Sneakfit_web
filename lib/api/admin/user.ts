2fdbcd8b4d2267df9e46a347da704acd9594c03f
SushantshtL0L <shtsushant2027@gmail.com>
2026-02-14 17:43:47 +0545

    Sprint-5: update admin user API calls

--- lib/api/admin/user.ts
+++ lib/api/admin/user.ts
@@ -15,3 +15,53 @@ export const createUser = async (userData: any) => {
     );
   }
 };
+
+export const getAllUsers = async (page: number = 1, limit: number = 10, role: string = 'all') => {
+  try {
+    const response = await axios.get(API.ADMIN.USER.LIST, {
+      params: { page, limit, role },
+    });
+    return response.data;
+  } catch (error: Error | any) {
+    throw new Error(
+      error.response?.data?.message || error.message || "Fetch users failed",
+    );
+  }
+};
+
+export const getUserById = async (id: string) => {
+  try {
+    const response = await axios.get(API.ADMIN.USER.GET(id));
+    return response.data;
+  } catch (error: Error | any) {
+    throw new Error(
+      error.response?.data?.message || error.message || "Fetch user failed",
+    );
+  }
+};
+
+export const updateUser = async (id: string, userData: any) => {
+  try {
+    const response = await axios.put(API.ADMIN.USER.UPDATE(id), userData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  } catch (error: Error | any) {
+    throw new Error(
+      error.response?.data?.message || error.message || "Update user failed",
+    );
+  }
+};
+
+export const deleteUser = async (id: string) => {
+  try {
+    const response = await axios.delete(API.ADMIN.USER.DELETE(id));
+    return response.data;
+  } catch (error: Error | any) {
+    throw new Error(
+      error.response?.data?.message || error.message || "Delete user failed",
+    );
+  }
+};
