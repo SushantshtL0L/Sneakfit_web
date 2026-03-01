34258da51319642bb32d522690e6425f32dbe136
SushantshtL0L <shtsushant2027@gmail.com>
2026-03-01 23:19:30 +0545

    Adjust public layout

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,9 +1,6 @@
-import Header from "./_components/Header";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <section className="min-h-screen flex flex-col">
-            <Header />
+        <section className="min-h-screen flex flex-col bg-black">
             <main className="flex-1">
                 {children}
             </main>
